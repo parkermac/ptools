be8e35f5267a47b390d3599eda146dc2c493d2b7
parkermac <p.maccready@gmail.com>
2017-09-03 14:29:18 -0700

    tide
    small edits to moon_orbit.py.  AND this is a test of pushing from my macbook instead of only pushing from my macbook pro.

--- tide/moon_orbit.py
+++ tide/moon_orbit.py
@@ -16,30 +16,30 @@ reload(tfun)
 
 year = 2016
 dt0 = datetime(year,1,1,tzinfo=pytz.timezone('UTC'))
-dt1 = datetime(year,12,31,tzinfo=pytz.timezone('UTC'))
+dt1 = datetime(year+19,12,31,tzinfo=pytz.timezone('UTC'))
+
+def add_tf(df):
+    # add the (normalized) tractive force
+    r = 1000 * df['Distance (km)'].values # distance in m
+    tf = tfun.get_tractive_scale(r)
+    tfn = tf/tf.mean() # normalize tractive force by its mean
+    df['Tractive Force'] = tfn
+    return df
 
 moon_orbit_df = efun.get_moon_orbit(dt0, dt1)
-
-r = 1000 * moon_orbit_df['Distance (km)'].values # distance in m
-tf = tfun.get_tractive_scale(r)
-tfp = 100 * (tf/tf.mean() - 1)
-
-moon_orbit_df['Tractive Force (% from mean)'] = tfp
+moon_robit_df = add_tf(moon_orbit_df)
 
 fm_df, nm_df = efun.get_full_new(dt0, dt1)
-a = pd.concat([fm_df, nm_df])
-a = a.sort_index()
 
 # plotting
 plt.close('all')
 
-moon_orbit_df.loc[:,['Declination (deg)', 'Tractive Force (% from mean)']].plot(grid=True)
+moon_orbit_df.loc[:,['Declination (deg)', 'Tractive Force']].plot(subplots=True, grid=True)
 
-if True:
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    a['Distance (km)'][a['fullnew']==1].plot(style='-or', ax=ax)
-    a['Distance (km)'][a['fullnew']==0].plot(style='-ob', ax=ax)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fm_df.plot(y='Distance (km)', label='Full Moon Distance (km)', style='-or', ax=ax)
+nm_df.plot(y='Distance (km)', label='New Moon Distance (km)', style='-ob', ax=ax)
 
 plt.show()
 
