a725626c169bfb53258a98bad5fff118bda62bbd
Parker MacCready <p.maccready@gmail.com>
2022-07-13 16:35:38 -0700

    Update test_pcolormesh.py

--- tests_examples/test_pcolormesh.py
+++ tests_examples/test_pcolormesh.py
@@ -13,22 +13,25 @@ zz = xx**2 + yy**2
 # plotting
 plt.close()
 
-fig = plt.figure(figsize=(20,5))
+fig = plt.figure(figsize=(20,8))
 
-ax0 = fig.add_subplot(131)
-ax0.pcolormesh(xx,yy,zz)
+ax = fig.add_subplot(131)
+cs = ax.pcolormesh(xx,yy,zz)
+fig.colorbar(cs, ax=ax, orientation='horizontal')
 # Result: this aligns the LOWER LEFT corner of each colored square
 # with the axes, and DROPS the last row and column of the data.
 
-ax1 = fig.add_subplot(132)
-ax1.pcolormesh(xx,yy,zz[:-1, :-1])
+ax = fig.add_subplot(132)
+ax.pcolormesh(xx,yy,zz[:-1, :-1])
+fig.colorbar(cs, ax=ax, orientation='horizontal')
 # Result: Here we have axes with size one larger than the data, and
 # the colored squares are plotted as if the axes referred to the CORNERS
 # of each square.  The plot is IDENTICAL to that of ax0 - so it is not doing
 # bilinear interpolation.
 
-ax2 = fig.add_subplot(133)
-ax2.pcolormesh(xx,yy,zz, shading='gouraud')
+ax = fig.add_subplot(133)
+ax.pcolormesh(xx,yy,zz, shading='gouraud')
+fig.colorbar(cs, ax=ax, orientation='horizontal')
 # Result: I'm not sure what this does.
 
 # A question to answer is how best to plot masked arrays.
